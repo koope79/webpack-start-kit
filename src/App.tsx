86e9e0cb91c5288f5e4075d705b2f9bab2bb85ff
koope79 <70471003+koope79@users.noreply.github.com>
2024-03-13 22:55:36 +0300

    add classnames

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ import './styles/index.scss';
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { useTheme } from "./theme/useTheme";
+import classNames from "classnames";
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', theme)}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about '}>О сайте</Link>
